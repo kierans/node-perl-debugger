adf6cc7de7a05d7e30f12d64801c41b36fc5cd59
Kieran Simpson <kierans777@gmail.com>
2015-08-08 17:17:22 +1000

    Fixed entry point for users of the module.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  DebuggerHost: require("./DebuggerHost"),
+  DebuggerCommands: require("./DebuggerCommands"),
+  DebuggerParser: require("./DebuggerParser")
+};
